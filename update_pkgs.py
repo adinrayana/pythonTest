0ecf57399d57e12569fb4458eb93594b227314e4
m.narayana@globallogic.com <155962654+adinrayana@users.noreply.github.com>
2024-04-15 19:05:35 +0530

    Update update_pkgs.py

--- update_pkgs.py
+++ update_pkgs.py
@@ -45,5 +45,5 @@ if __name__ == "__main__":
     pkg_name = "IAM_Python_SDK"
     versions = ["0.1", "0.2", "0.3.dev0"]
     short_desc = 'A public github-hosted repo, with a dependency to another package.'
-    homepage = 'hhttps://github.com/hitachi-genai/platform-security-iam-python-sdk/tree/python-sdk-push'
+    homepage = 'https://github.com/hitachi-genai/platform-security-iam-python-sdk/tree/python-sdk-push'
     execute_main(pkg_name, versions, short_desc, homepage)
